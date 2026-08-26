a815133fc67c7e7c879d279d02e108d43f481527
phuoctung28 <nguyenphuoctung.284@gmail.com>
2026-08-26 15:15:27 +0700

    migration screep

--- migrations/versions/20260823000003_merge_outbox_and_serving_label_heads.py
+++ migrations/versions/20260823000003_merge_outbox_and_serving_label_heads.py
@@ -2,10 +2,10 @@
 
 from collections.abc import Sequence
 
-revision: str = "20260823000002"
+revision: str = "20260823000003"
 down_revision: tuple[str, str] = (
     "20260822000001",
-    "20260823000001",
+    "20260823000002",
 )
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
